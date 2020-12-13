c04bb9e1e98001b1bf75db93a3a097f94697d4fd
Grant <gpsiegel@hotmail.com>
2020-12-13 16:43:40 -0600

    Update indeed_scrape.py
    Added more job choices, increased the "fromage" attribute on the url from "1" to "3" to cover more results.  Added IndexError handling in main(). Added writing to txt portion for the scraping info to append to with append mode enabled on the "to_csv"

--- indeed_scrape.py
+++ indeed_scrape.py
@@ -1,29 +1,30 @@
 import random
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from time import sleep
 
 def random_job_listing():
-    """returns a random tech-related job from list"""
     jobs = ["cloud", "linux", "python", "kubernetes", \
-    "information security", "IoT", "solutions architect", \
-    "data science", "azure", "aws", "gcp", "devops", \
-    "software developer", "backend developer", "front end"]
+    "information+security", "IoT", "solutions+architect", \
+    "data+science", "azure", "aws", "gcp", "devops", \
+    "software+developer", "backend+developer", "front+end", \
+    "java", "c++", "javascript", "serverless", "it+support", "mongodb", \
+    "node.js", "vmware", "active+directory", "terraform", "CCNA"]
     rando = ''
     rando = random.choice(jobs)
     return rando
 
 def random_tx_city():
-    """returns a random major Texas city"""
     cities = ["dallas", "fort+worth", "austin", "el+paso", "houston", "san+antonio"]
     rdm_city = ''
     rdm_city = random.choice(cities)
     return rdm_city
 
 def get_tx_url(job, city):
-    """random job and city for the link to get the records needed"""
-    #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
-    tx_url = f'https://www.indeed.com/jobs?q={job}&l={city}%2C%20tx&fromage=1'
-    r = requests.get(tx_url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    tx_url = f'https://www.indeed.com/jobs?q={job}&l={city}%2C%20tx&fromage=3'
+    r = requests.get(tx_url, headers)
     soup = BeautifulSoup(r.text, 'html.parser')
     jobcards = soup.find_all('div', 'jobsearch-SerpJobCard')
     card = jobcards[0]
@@ -35,13 +36,32 @@ def get_tx_url(job, city):
         salary = card.find('span', 'salaryText').text.strip()
     except AttributeError:
         salary = ''
-    record = (job_title, company, location, tx_url)
-    print(record)
+    indeed_results = {
+        'job title': job_title,
+        'company': company,
+        'location': location,
+        'salary': salary,
+        'link': tx_url
+    }
+    job_result = []
+
+    job_result.append(indeed_results)
+    df = pd.DataFrame(job_result)
+    print(df.head())
+    df.to_csv('jobs.txt', header=None, index=None, sep=' ', mode='a')
 
 def main():
-    job = random_job_listing()
-    city = random_tx_city()
-    tx_url_made = get_tx_url(job, city)
-
+    for x in range(0, 1):
+        try:
+            job = random_job_listing()
+            city = random_tx_city()
+            tx_url_made = get_tx_url(job, city)
+        except IndexError:
+            continue
+        if IndexError:
+            sleep(1)
+        else:
+            break
+   
 if __name__ == '__main__':
     main()
